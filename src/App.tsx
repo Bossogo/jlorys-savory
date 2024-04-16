23e4c8226715c0424ad7cb671c5ff98948586fc6
Ogooluwa David Ilori <ilori.david@stu.cu.edu.ng>
2024-04-16 13:24:09 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
