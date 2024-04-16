0e017ce8cbfecd77677dc2aeca4a21561c5d5d57
Ogooluwa David Ilori <ilori.david@stu.cu.edu.ng>
2024-04-16 14:50:41 +0100

    setting up

---
+++ src/App/App.tsx
@@ -0,0 +1,21 @@
+import "./App.scss"
+import * as bootstrap from 'bootstrap'
+import { useState } from 'react';
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="offset-1 col-10">
+            <h1>React Typescript Bootstrap</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
